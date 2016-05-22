9b93a58414a808769a2084735c368594cbdf181b
Mikira Sora <MikiraSora0409@126.com>
2016-05-22 10:18:38 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.util.regex.*;
+import java.io.*;
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+		
+		try{
+		FileInputStream file=new FileInputStream("/sdcard/ee.h");
+		byte[] buf=new byte[file.available()];
+		Reader r=new InputStreamReader(file);
+		CharSequence chars="";
+		int c;//=(char)-1;
+		while((c=r.read())!=-1)
+		  chars+=String.valueOf((char)c);
+		System.out.println(chars);
+		file.close();
+		
+		Lrc_Parser lp=new Lrc_Parser(new Lrc_Parser_Option(),new Lrc_Parser_Expr());
+		Lrc_Parser_Result res=lp.Decode(chars.toString(),null);
+		System.out.println("test successfully.");
+		System.out.println("Title : "+res.Title);
+		System.out.println(("Artist : "+res.Artist));
+		System.out.println("Lyric Length : "+res.Lyric.length());
+		System.out.println(res.Lyric);
+			
+		}catch(Exception e){e.fillInStackTrace();System.out.println(e);}
+		System.out.println("end");
+		}
+}
